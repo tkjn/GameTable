5c46121fb82204b4f131ea1447bb5c7368cb10a4
Derek Kaye <github@dezzanet.co.uk>
2014-05-26 16:02:06 +0100

    Set preferred gold size width only - ignore the height

--- src/uk/co/dezzanet/gametable/charactersheet/CharacterSheetPanel.java
+++ src/uk/co/dezzanet/gametable/charactersheet/CharacterSheetPanel.java
@@ -92,8 +92,8 @@ public class CharacterSheetPanel extends JPanel implements ICharacterDataChanged
 		SpinnerModel gold_model = new SpinnerNumberModel(0, 0, Integer.MAX_VALUE, 1);
 		gold = new JSpinner(gold_model);
 		gold.addChangeListener(new GoldChangeListener());
-		Dimension gold_size = new Dimension();
-		gold_size.setSize(70, wounds_size.getHeight());
+		Dimension gold_size = new Dimension(gold.getPreferredSize());
+		gold_size.width = 70;
 		gold.setPreferredSize(gold_size);
 		add(gold);
 		
