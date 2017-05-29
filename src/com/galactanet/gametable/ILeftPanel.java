3c07b0e8c35517bac744981efca2ed010167c4e2
Derek Kaye <github@dezzanet.co.uk>
2017-05-29 20:18:28 +0100

    Add ILeftPanel interface and make CharacterSheetPlugin implement it

---
+++ src/com/galactanet/gametable/ILeftPanel.java
@@ -0,0 +1,7 @@
+package com.galactanet.gametable;
+
+import javax.swing.*;
+
+public interface ILeftPanel {
+    JPanel getLeftPanel();
+}
--- src/uk/co/dezzanet/gametable/charactersheet/CharacterSheetPlugin.java
+++ src/uk/co/dezzanet/gametable/charactersheet/CharacterSheetPlugin.java
@@ -2,15 +2,19 @@ package uk.co.dezzanet.gametable.charactersheet;
 
 import com.galactanet.gametable.GametableFrame;
 import com.galactanet.gametable.IGametablePlugin;
+import com.galactanet.gametable.ILeftPanel;
 
-public class CharacterSheetPlugin implements IGametablePlugin {
+import javax.swing.*;
+
+public class CharacterSheetPlugin implements IGametablePlugin, ILeftPanel {
 
     private static final String PLUGIN_NAME = "Character sheet";
     private static final String PANEL_TITLE = "Character Sheet";
+    private CharacterSheetPanel panel;
 
     @Override
     public void initialise(GametableFrame gametable) {
-        CharacterSheetPanel panel = new CharacterSheetPanel();
+        panel = new CharacterSheetPanel();
         gametable.addPanelToLeftPane(panel, PANEL_TITLE);
         gametable.registerAutoSaveListener(new AutoSaveListener(panel.getStorage()));
         gametable.getEventDispatcher().listenForPogMenuRender(new PogMenuRenderListener(panel.getPogAdapter()));
@@ -19,5 +23,10 @@ public class CharacterSheetPlugin implements IGametablePlugin {
     @Override
     public String getName() {
         return PLUGIN_NAME;
+    }
+
+    @Override
+    public JPanel getLeftPanel() {
+        return panel;
     }
 }
