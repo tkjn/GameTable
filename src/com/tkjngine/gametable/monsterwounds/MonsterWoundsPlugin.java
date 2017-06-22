1684d00b3986e257ae3b365933819b1b3a694ce9
tkjn <tkjn@tkjngine.com>
2017-06-22 21:21:25 +0100

    Tidy up unnecessary code from monster wounds plugin

--- src/com/tkjngine/gametable/monsterwounds/MonsterWoundsPlugin.java
+++ src/com/tkjngine/gametable/monsterwounds/MonsterWoundsPlugin.java
@@ -2,37 +2,20 @@ package com.tkjngine.gametable.monsterwounds;
 
 import com.galactanet.gametable.GametableFrame;
 import com.galactanet.gametable.IGametablePlugin;
-import com.galactanet.gametable.ILeftPanelProvider;
 
 import javax.swing.*;
 
-public class MonsterWoundsPlugin implements IGametablePlugin, ILeftPanelProvider {
+public class MonsterWoundsPlugin implements IGametablePlugin {
 
     private static final String PLUGIN_NAME = "Monster Wounds";
-    private static final String PANEL_TITLE = "Monster Wounds";
-    //private MonsterWoundsPanel panel;
 
     @Override
     public void initialise(GametableFrame gametable) {
-        // TODO
-        //gametable.registerLeftPanelProvider(this);
-        //gametable.registerAutoSaveListener(new AutoSaveListener(panel.getStorage()));
         gametable.getEventDispatcher().listenForPogMenuRender(new PogMenuRenderListener());
     }
 
     @Override
     public String getName() {
         return PLUGIN_NAME;
-    }
-
-    @Override
-    public JPanel getLeftPanel() {
-        // TODO
-        return new JPanel();
-    }
-
-    @Override
-    public String getPanelTitle() {
-        return PANEL_TITLE;
     }
 }
