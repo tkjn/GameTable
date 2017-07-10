965eae873cda2b57722af4a0ac921440dbd89f75
Derek Kaye <github@dezzanet.co.uk>
2017-07-10 16:32:19 +0100

    Update to remove method name

--- gametable.sh
+++ gametable.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp 'gametable.jar:plugins/*' -Xmx1024M -Xms512M com.galactanet.gametable.GametableApp.main
+java -cp 'gametable.jar:plugins/*' -Xmx1024M -Xms512M com.galactanet.gametable.GametableApp
